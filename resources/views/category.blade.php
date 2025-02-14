92546c6232e8aaaec99535833a565c6d2f8df217
230752 <230752@student.glu.nl>
2025-02-14 10:51:01 +0100

    Enhance category page layout with improved button styles and add menu items

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -3,40 +3,46 @@
 @section('title', 'category page')
 
 @section('header')
-<div id="header-content" class="shadow-lg">
-    <img src="{{asset('img/happy_logo.png')}}" alt="logo" class="w-32">
-</div>
+    <div id="header-content" class="shadow-lg">
+        <img src="{{asset('img/happy_logo.png')}}" alt="logo" class="w-40">
+    </div>
 @endsection
 
 @section('content')
-<div id="content-container" class="pt-12">
-    <button class="bg-white text-black px-4 py-2 shadow-lg">Take away</button>
-    <button class="bg-white text-black px-4 py-2 shadow-lg">Eat in</button>
-</div>
+    <div id="content-container" class="pt-12">
+        <div id="option-btn-container">
+            <button class="bg-white text-black text-4xl px-4 py-2 shadow-lg">Take away</button>
+            <button class="bg-white text-black text-4xl px-4 py-2 shadow-lg">Eat in</button>
+        </div>
+        <div id="category-container" class="flex flex-row flex-wrap gap-10">
+            <div id="" class="mt-10 border-none w-1/4 bg-white flex flex-col justify-end">
+                <img src="{{asset('img/test.png')}}" alt="" class="w-full">
+                <p class="text-4xl text-center p-4">Make it a Menu</p>
+            </div>
+            <div id="" class="mt-10 border-none w-1/4 bg-white flex flex-col justify-end">
+                <img src="{{asset('img/test.png')}}" alt="" class="w-full">
+                <p class="text-4xl text-center p-4">Make it a Menu</p>
+            </div>
+            <div id="" class="mt-10 border-none w-1/4 bg-white flex flex-col justify-end">
+                <img src="{{asset('img/test.png')}}" alt="" class="w-full">
+                <p class="text-4xl text-center p-4">Make it a Menu</p>
+            </div>
+            <div id="" class="mt-10 border-none w-1/4 bg-white flex flex-col justify-end">
+                <img src="{{asset('img/test.png')}}" alt="" class="w-full">
+                <p class="text-4xl text-center p-4">Make it a Menu</p>
+            </div>
+            <div id="" class="mt-10 border-none w-1/4 bg-white flex flex-col justify-end">
+                <img src="{{asset('img/test.png')}}" alt="" class="w-full">
+                <p class="text-4xl text-center p-4">Make it a Menu</p>
+            </div>
+            <div id="" class="mt-10 border-none w-1/4 bg-white flex flex-col justify-end">
+                <img src="{{asset('img/test.png')}}" alt="" class="w-full">
+                <p class="text-4xl text-center p-4">Make it a Menu</p>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('footer')
-<h1>test</h1>
-<h1>test</h1>
-<h1>test</h1>
-<h1>test</h1>
-<h1>test</h1>
-<h1>test</h1>
-<h1>test</h1>
-<h1>test</h1>
-<h1>test</h1>
-<h1>test</h1>
-<h1>test</h1>
-<h1>test</h1>
-<h1>test</h1>
-<h1>test</h1>
-<h1>test</h1>
-<h1>test</h1>
-<h1>test</h1>
-<h1>test</h1>
-<h1>test</h1>
-<h1>test</h1>
-<h1>test</h1>
-<h1>test</h1>
-
+    <h1>footer</h1>
 @endsection
