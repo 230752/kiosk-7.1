6dc27bbd0776b0f6d6b062484ea580ce6464f7a7
230752 <230752@student.glu.nl>
2025-03-06 11:52:30 +0100

    winkelmand cookie

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,13 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Cookie;
 
 class CartController extends Controller {
-    public function addProduct() {
-        // Cookie::queue()
-        echo 'test';
+    public function addProduct(Request $request, $product_id) {
+        $quantity = 1;
+
+        $cart = json_decode(Cookie::get('cart', '[]'), true);
+
+        $productExists = false;
+
+        foreach ($cart as &$item) {
+            if ($item['product_id'] == $product_id) {
+                $item['quantity'] += $quantity;
+                $productExists = true;
+                break;
+            }
+        }
+
+        if (!$productExists) {
+            $cart[] = [
+                'product_id' => $product_id,
+                'quantity' => $quantity
+            ];
+        }
+
+        Cookie::queue('cart', json_encode($cart), 5);
+
+        return Response()->json(['cart' => $cart]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\PreferenceController;
 use App\Http\Controllers\ShopController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/set-preference/{preference}', [PreferenceController::class, 'setPreference'])->name('set_preference');
+
+Route::get('/add-product/{product_id}', [CartController::class, 'addProduct'])->name('add_product');
 
 Route::get('/', function () {
     return view('Add');
