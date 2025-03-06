14961a3b1afb84367304c2a5c796672bae90d400
230752 <230752@student.glu.nl>
2025-03-06 09:45:22 +0100

    cookies

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CookieController;
+
+Route::get('/set-preference/{preference}', [CookieController::class, 'setPreference'])->name('set_preference');
+
 Route::get('/', function () {
-    return view ('Add');
+    return view('Add');
 });
 
 Route::get('/start', function () {
     return view('start');
-}) ->name('start');
+})->name('start');
 
 Route::get('/category', function () {
-    return view ('category');
-}) ->name('category');
+    return view('category');
+})->name('category');
 
 Route::get('/main', function () {
-    return view ('main');
-}) ->name('main');
+    return view('main');
+})->name('main');
 
 Route::get('/shop', function () {
-    return view ('shop');
-}) ->name('shop');
+    return view('shop');
+})->name('shop');
